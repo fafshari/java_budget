748bee67eda22dc7d7a42a25545e3cfce236adc6
Ryan Jayakody <ryanjayako@gmail.com>
2012-12-07 17:09:33 -0500

    POJOs

--- src/sheridan/BudgetUser.java
+++ src/sheridan/BudgetUser.java
@@ -4,7 +4,33 @@ public class BudgetUser {
 
 	String mName;
 	String mEmail;
-	double totalCash;
+	double mtotalCash;
+	
+	public BudgetUser(String name, String email, double totalCash) {
+		mName = name;
+		mEmail = email;
+		mtotalCash = totalCash;
+	}
+
+	
+	public String getmName() {
+		return mName;
+	}
+	public String getmEmail() {
+		return mEmail;
+	}
+	public double getTotalCash() {
+		return mtotalCash;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+	public void setTotalCash(double totalCash) {
+		this.mtotalCash = totalCash;
+	}
 	
 	
 }
