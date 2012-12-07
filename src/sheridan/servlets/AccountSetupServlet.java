b627ade70453ae446cc1ef4c1acbd3852648c9f4
Farzad Afshari <farzad.afshari@wickedroar.com>
2012-12-07 17:20:19 -0500

    AccountSetupServlet doPost functionality

--- src/sheridan/servlets/AccountSetupServlet.java
+++ src/sheridan/servlets/AccountSetupServlet.java
@@ -1,11 +1,13 @@
 package sheridan.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class AccountSetupServlet
@@ -13,27 +15,82 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/AccountSetupServlet")
 public class AccountSetupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccountSetupServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public AccountSetupServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		BudgetUser user;
+		HttpSession session = request.getSession();
+
+		String name = request.getParameter("name");
+		float totalCash;
+		String email = request.getParameter("email");
+
+		boolean hasError = false;
+
+		try {
+			totalCash = Float.parseFloat(request.getParameter("totalCash"));
+			session.setAttribute("totalCashError", null);
+		} catch (NumberFormatException e) {
+
+			session.setAttribute("totalCashError", "unit price is not a number");
+			hasError = true;
+		} catch (NullPointerException e) {
+
+			session.setAttribute("totalCashError",
+					"please enter the unit price");
+			hasError = true;
+		}
+
+		if (name != null && !"".equals(name)) {
+			user.setName(name);
+			session.setAttribute("nameError", null);
+		} else {
+			session.setAttribute("nameError", "please enter the address");
+			user.setName("");
+			hasError = true;
+		}
+
+		if (email != null && !"".equals(email)) {
+			user.setEmail(email);
+			session.setAttribute("emailError", null);
+		} else {
+			session.setAttribute("emailError", "please enter the shipment date");
+			user.setEmail("");
+			hasError = true;
+		}
+
+		session.setAttribute("user", user);
+
+		if (hasError) {
+			response.sendRedirect("HomePage.jsp");
+
+			return;
+		}
+
+		response.sendRedirect("SystemPage.jsp");
+
 	}
 
 }
